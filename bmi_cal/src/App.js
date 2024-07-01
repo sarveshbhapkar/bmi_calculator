9321657a9447494b84459bcdd0413ca8329138d6
sarveshbhapkar <sarveshbhapkar100@gmail.com>
2024-07-01 11:23:22 +0530

    created form

--- bmi_cal/src/App.js
+++ bmi_cal/src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h2> BMI CALCULATOR</h2>
+        <form>
+          <div>
+            <label>weight{lbs}</label>
+            <input
+              type="text"
+              placeholder="Enter Weight Value"
+              value={weight}
+            />
+          </div>
+
+          <div>
+            <label>height{inches}</label>
+            <input
+              type="text"
+              placeholder="Enter Height Value"
+              value={height}
+            />
+          </div>
+
+          <div>
+            <button className="btn" type="submit">
+              Submit
+            </button>
+            <button className="btn btn-outline" onClick={reload} type="submit">
+              Reload
+            </button>
+          </div>
+
+          <div className="center">
+            <h3>Your BMI is :{bmi} </h3>
+            <p>{message}</p>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
